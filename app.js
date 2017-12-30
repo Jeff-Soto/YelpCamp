28f436a53492f28aea18ab7230ec0a000d82cc88
Jeff Soto <info@sotowebstudios.com>
2017-12-30 20:52:21 +0000

    Add create route, install body parser, update new view and route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express"),
       mongoose = require("mongoose"),
       Campground = require("./models/campground"),
+      bodyParser = require("body-parser"),
       app = express();
       
 mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/yelp_camp");
@@ -8,9 +9,15 @@ mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views/partials"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Root route
+app.get("/", (req, res)=>{
+    res.redirect("/campgrounds");
+});
  
 // INDEX ROUTE     
-app.get("/", (req, res)=>{
+app.get("/campgrounds", (req, res)=>{
     Campground.find({}, (err, allCampgrounds)=>{
         if (err){
             console.log("Something went wrong when finding all campgrounds.", err);
@@ -21,8 +28,26 @@ app.get("/", (req, res)=>{
 });
 
 // NEW ROUTE
-app.get("/new", (req, res)=>{
+app.get("/campgrounds/new", (req, res)=>{
     res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/campgrounds", (req, res)=>{
+    const newCampData = {
+        name: req.body.name,
+        description: req.body.description
+    };
+    console.log(newCampData);
+    Campground.create( newCampData, (err, newCampground)=>{
+        if (err){
+            console.log("Error detected when creating new campground: ", err);
+            res.redirect("/campgrounds");
+        } else{
+            console.log("Successfully created campground: ",newCampground);
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 
