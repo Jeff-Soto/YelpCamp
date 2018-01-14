6eec4068dec4c7402e4e0de1e0b05666d03e8905
Jeff Soto <info@sotowebstudios.com>
2018-01-14 17:44:21 +0000

    add edit,update,destroy routes for comment. Create edit view for comments

--- routes/comments.js
+++ routes/comments.js
@@ -22,19 +22,49 @@ router.post("/", (req, res)=>{
            console.log("Error finding campground: ",err);
        } else{
            Comment.create(req.body.comment, (err, comment) =>{
-                   if (err){
-                       console.log("Error creating comment: ", err);
-                   } else{
-                       comment.author.id = req.user._id;
-                       comment.author.username = req.user.username;
-                       comment.save();
-                       campground.comments.push(comment._id);
-                       campground.save();
-                       res.redirect("/campgrounds/"+campground._id);
-                   }
+               if (err){
+                   console.log("Error creating comment: ", err);
+                   
+               } else{
+                   comment.author.id = req.user._id;
+                   comment.author.username = req.user.username;
+                   comment.save();
+                   campground.comments.push(comment._id);
+                   campground.save();
+                   res.redirect("/campgrounds/"+campground._id);
+               }
            });
        }
    });
+});
+
+router.get("/:commentId/edit", (req, res) => {
+    Comment.findById(req.params.commentId, (err, comment)=>{
+        if (err){
+            console.log("Err finding comment by id", err);
+        } else {
+            res.render("comments/edit", {comment: comment, campground_id: req.params.id});
+        }
+    });
+});
+
+router.put("/:commentId", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.commentId, req.body.comment, (err, updatedComment)=>{
+        if (err){
+            console.log("err updating comment.", err);
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+router.delete("/:commentId", (req, res)=>{
+    Comment.findByIdAndRemove(req.params.commentId, (err)=>{
+        if (err){
+            console.log("Error deleting comment: ", err);
+        }
+        res.redirect("/campgrounds/" + req.params.id);
+    });
 });
 
 module.exports = router;
